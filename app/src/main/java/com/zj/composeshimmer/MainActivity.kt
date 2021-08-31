950dd67536c2f82297ec43a05514d2918d57cea4
shenzhen2017 <2868405029@qq.com>
2021-08-31 08:14:34 +0800

    ADD: shimmer effect

--- app/src/main/java/com/zj/composeshimmer/MainActivity.kt
+++ app/src/main/java/com/zj/composeshimmer/MainActivity.kt
@@ -3,16 +3,20 @@ package com.zj.composeshimmer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.google.accompanist.placeholder.placeholder
+import com.google.accompanist.placeholder.material.placeholder
 import com.zj.composeshimmer.ui.theme.ComposeShimmerTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,14 +35,45 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    var loading: Boolean by remember { mutableStateOf(true) }
-    Text(text = "Hello $name!", modifier = Modifier
-        .padding(16.dp)
-        .shimmer(visible = loading)
-        .clickable {
-            loading = !loading
+    var loading: Boolean by remember {
+        mutableStateOf(true)
+    }
+
+    Row(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+            .shimmer(true), verticalAlignment = Alignment.CenterVertically
+    ) {
+        Spacer(
+            modifier = Modifier
+                .width(100.dp)
+                .height(100.dp)
+                .placeholder(true)
+        )
+        Spacer(modifier = Modifier.width(20.dp))
+        Column(modifier = Modifier.padding(8.dp)) {
+            Spacer(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(30.dp)
+                    .placeholder(true)
+            )
+            Spacer(
+                modifier = Modifier
+                    .padding(0.dp,8.dp)
+                    .fillMaxWidth()
+                    .height(30.dp)
+                    .placeholder(true)
+            )
+            Spacer(
+                modifier = Modifier
+                    .fillMaxWidth(0.5f)
+                    .height(30.dp)
+                    .placeholder(true)
+            )
         }
-    )
+    }
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/zj/composeshimmer/Shimmer.kt
+++ app/src/main/java/com/zj/composeshimmer/Shimmer.kt
@@ -1,38 +1,117 @@
 package com.zj.composeshimmer
 
+import androidx.compose.animation.core.*
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.draw.DrawModifier
-import androidx.compose.ui.draw.drawWithContent
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.*
 import androidx.compose.ui.graphics.drawscope.ContentDrawScope
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.layout.LayoutModifier
 import androidx.compose.ui.layout.Measurable
 import androidx.compose.ui.layout.MeasureResult
 import androidx.compose.ui.layout.MeasureScope
 import androidx.compose.ui.unit.Constraints
+import androidx.core.graphics.transform
 
-fun Modifier.shimmer(
-    visible: Boolean
-): Modifier = composed {
-    drawWithContent {
-        drawContent()
-        if (visible) {
-            drawRect(color = Color.Blue)
-        }
-    }
+//fun Modifier.shimmer(
+//    visible: Boolean
+//): Modifier = composed {
+//    drawWithContent {
+//        drawContent()
+//        if (visible) {
+//            drawRect(color = Color.Blue)
+//        }
+//    }
+//}
+
+//fun Modifier.shimmer(visible: Boolean): Modifier = this.then(
+//
+//    ShimmerModifier(visible = visible)
+//)
+
+fun Modifier.shimmer(visible: Boolean): Modifier = composed {
+    val infiniteTransition = rememberInfiniteTransition()
+    val progress by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 1.0f,
+        animationSpec = infiniteRepeatable(
+            tween(
+                durationMillis = 1300,
+                delayMillis = 300,
+                easing = LinearEasing
+            ),
+            repeatMode = RepeatMode.Restart
+        )
+    )
+    ShimmerModifier(visible = visible, progress = progress)
 }
 
-internal class ShimmerModifier(visible: Boolean) : DrawModifier, LayoutModifier {
+internal class ShimmerModifier(
+    private val visible: Boolean,
+    private val baseAlpha: Float = 0.3f,
+    private val highlightAlpha: Float = 0.9f,
+    private val intensity: Float = 0f,
+    private val dropOff: Float = 0.5f,
+    private val progress: Float
+) : DrawModifier, LayoutModifier {
+    private val shaderColors = listOf(
+        Color.Unspecified.copy(alpha = baseAlpha),
+        Color.Unspecified.copy(alpha = highlightAlpha),
+        Color.Unspecified.copy(alpha = highlightAlpha),
+        Color.Unspecified.copy(alpha = baseAlpha)
+    )
+    private val colorStops: List<Float> = listOf(
+        ((1f - intensity - dropOff) / 2f).coerceIn(0f, 1f),
+        ((1f - intensity - 0.001f) / 2f).coerceIn(0f, 1f),
+        ((1f + intensity + 0.001f) / 2f).coerceIn(0f, 1f),
+        ((1f + intensity + dropOff) / 2f).coerceIn(0f, 1f)
+    )
+    private val cleanPaint = Paint()
+    private val paint = Paint().apply {
+        isAntiAlias = true
+        style = PaintingStyle.Fill
+        blendMode = BlendMode.DstIn
+    }
+
     override fun ContentDrawScope.draw() {
-        TODO("Not yet implemented")
+        drawIntoCanvas {
+            it.withSaveLayer(Rect(0f, 0f, size.width, size.height), paint = cleanPaint) {
+                drawContent()
+                if (visible) {
+                    val dx = -(size.width) + (size.width * 2 * progress)
+                    paint.shader?.transform {
+                        reset()
+                        postTranslate(dx, 0f)
+                    }
+                    it.drawRect(Rect(0f, 0f, size.width, size.height), paint = paint)
+                }
+            }
+        }
     }
 
     override fun MeasureScope.measure(
         measurable: Measurable,
         constraints: Constraints
     ): MeasureResult {
-        TODO("Not yet implemented")
+        val placeable = measurable.measure(constraints)
+        val size = Size(width = placeable.width.toFloat(), height = placeable.height.toFloat())
+        updateSize(size)
+        return layout(placeable.width, placeable.height) {
+            placeable.place(0, 0)
+        }
     }
 
+    private fun updateSize(size: Size) {
+        paint.shader = LinearGradientShader(
+            Offset(0f, 0f),
+            Offset(size.width, 0f),
+            shaderColors,
+            colorStops
+        )
+    }
 }
